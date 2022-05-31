0b2ad0d4964a0a1caeca19002d7ebfb0b8b24fe6
Sagar@16 <sagarkumarpradhan2001@gmail.com>
2022-06-01 00:28:28 +0530

    navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
+import Logo from "../../public/main_logo.webp";
 
 export const Navbar = () => {
   return (
@@ -11,6 +12,16 @@ export const Navbar = () => {
         <Link to={"/"}>LOG IN</Link>
         <Link to={"/"}>CART()</Link>
         <SearchIcon className="nav-icon" />
+      </div>
+      <div className="nav-logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="nav-link">
+        <Link to={""}>BAND T-SHIRTS</Link>
+        <Link to={""}>ABOUT US</Link>
+        <Link to={""}>THE ARTISTS</Link>
+        <Link to={""}>WORLD CANCER DAY</Link>
+        <Link to={""}>PRESS</Link>
       </div>
     </div>
   );
