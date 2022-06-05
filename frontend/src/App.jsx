6027a20122b84b4b007b97237a5e2228187bce35
salove16 <shashank.shalabh909@gmail.com>
2022-06-06 02:01:46 +0530

    footer-update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/navbar/navbar";
-<<<<<<< HEAD
 import {Footer} from "./components/Footer/Footer"
-=======
 import { Collection } from "./pages/collections/collection";
 import { Home } from "./pages/home/home";
 import { useDispatch } from "react-redux";
 import { getData } from "./redux/data/action";
 import { SignUp } from "./pages/signup/signup";
 import { SignIn } from "./pages/signin/sign";
->>>>>>> c4d2454419fde0d6e55664c18c94138043bfad66
 
 function App() {
   const dispatch = useDispatch();
@@ -19,18 +16,17 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
       <Routes></Routes>
-      <Footer/>
-=======
+    
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/collections" element={<Collection />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
->>>>>>> c4d2454419fde0d6e55664c18c94138043bfad66
+      <Footer/>
     </div>
+
   );
 }
 
